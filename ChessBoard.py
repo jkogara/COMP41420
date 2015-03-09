288d766ac9a7a63c7a9df3275213914d7d8c0a71
John O'Gara <johnogara@gmail.com>
2015-03-09 14:48:16 +0000

    added meaning of life comment

--- ChessBoard.py
+++ ChessBoard.py
@@ -39,7 +39,7 @@ class ChessBoard:
     STALEMATE = 3
     FIFTY_MOVES_RULE = 4
     THREE_REPETITION_RULE = 5
-
+    # This line is the meaning of life
     # Special moves
     NORMAL_MOVE = 0
     EP_MOVE = 1
